058b60a71a9db2549d44890bdc74a9664bd2a805
Eliort Kid <eliotfonse99@gmail.com>
2025-08-13 18:50:58 -0300

    test get cockies

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,14 +1,15 @@
 import { auth } from "@/lib/auth";
 import Game from "./Game";
 import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default async function Page() {
   const session = await auth();
   if (!session) redirect("/sign-in");
 
   
-  // const cookieStore = await cookies(); // await necessário
-  // const token = cookieStore.get("authjs.session-token")?.value;
-
+  const cookieStore = await cookies(); // await necessário
+  const token = cookieStore.get("__Secure-authjs.session-token")?.value;
+  console.log("Token from cookies:", token);
   return <Game />;
 }
